e4898abb081d23c04e70ed81a7449a5c884492f9
AdamMcguigan <C00248515@itcarlow.ie>
2022-01-26 17:36:01 +0000

    Added a simple crosshair where the mouse is on screen

--- Project_Arena_Wars/Assets/Scripts/PlayerRotation.cs
+++ Project_Arena_Wars/Assets/Scripts/PlayerRotation.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+    
     void Update()
     {
+        
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 dir = Input.mousePosition - pos;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
---
+++ Project_Arena_Wars/Assets/Scripts/crosshair.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crosshair : MonoBehaviour
+{
+    private Vector3 MouseCoords;
+    public float MouseSensitivity = 0.1f;
+    void Update()
+    {
+        GameObject crosshair = GameObject.Find("crosshair");
+        MouseCoords = Input.mousePosition;
+        MouseCoords = Camera.main.ScreenToWorldPoint(MouseCoords);
+        crosshair.transform.position = Vector2.Lerp(transform.position, MouseCoords, MouseSensitivity);
+        print(MouseCoords);
+    }
+}
